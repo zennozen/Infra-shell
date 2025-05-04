250901b44e5d17ef9c6f82b9b3f6ce259306f339
min <min.tang@outlook.com>
2025-05-04 10:18:12 +0800

    update comments

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+########
+## Usage: curl -fsSL https://raw.githubusercontent.com/zennozen/Infra-shell/refs/heads/<branch_name>/run.sh | bash /dev/stdin <branch_name>
+########
 set -e
 
 GITHUB_PROXY="https://ghproxy.net"
 BRANCH=$1
 PROJ_URL="$GITHUB_PROXY/https://github.com/zennozen/Infra-shell/archive/refs/heads/$BRANCH.zip"
 
+which unzip || dnf install -y unzip
 wget -c $PROJ_URL && unzip $BRANCH.zip
 cd Infra-shell-$BRANCH && ls -l
 
