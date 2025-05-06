b944fce0264101bb1d89a464b203861817052b3e
min <min.tang@outlook.com>
2025-05-06 10:14:43 +0800

    fix rpm dep

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ GITHUB_PROXY="https://ghproxy.net"
 BRANCH=$1
 PROJ_URL="$GITHUB_PROXY/https://github.com/zennozen/Infra-shell/archive/refs/heads/$BRANCH.zip"
 
+which wget || dnf install -y wget
 which unzip || dnf install -y unzip
 wget -c $PROJ_URL && unzip $BRANCH.zip
 cd Infra-shell-$BRANCH && ls -l
