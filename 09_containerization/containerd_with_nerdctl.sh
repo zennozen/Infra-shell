8e9aed4b0b91dc4f30e1f24895913b766fcd16c7
min <min.tang@outlook.com>
2025-05-18 16:26:18 +0800

    update

--- 09_containerization/containerd_with_nerdctl.sh
+++ 09_containerization/containerd_with_nerdctl.sh
@@ -265,7 +265,7 @@ EOF
   systemctl status --no-pager containerd buildkit
 
   _logger info "5. Enable nerdctl command auto-completion"
-  dnf install -y bash-completion
+  dnf list installed | grep bash-completion || dnf install -y bash-completion
   echo "source <(nerdctl completion bash)" >> ~/.bashrc && source <(nerdctl completion bash)
 
   _print_line split -
